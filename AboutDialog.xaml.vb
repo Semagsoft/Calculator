381004707ab0af703e1d4f0749d9ca2fcf808117
Semagsoft <32625597+Semagsoft@users.noreply.github.com>
2025-12-19 10:25:44 -0600

    First commit

---
+++ AboutDialog.xaml.vb
@@ -0,0 +1,20 @@
+﻿Partial Public Class AboutDialog
+
+    Private Sub AboutDialog_Loaded(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles MyBase.Loaded
+        If GlassHelper.DwmIsCompositionEnabled Then
+            GlassHelper.ExtendGlassFrame(Me, New Thickness(-1, -1, -1, -1))
+        End If
+        NameLabel.Content = My.Application.Info.ProductName
+        VersionLabel.Content = "Version: " + My.Application.Info.Version.Major.ToString + "." + My.Application.Info.Version.Minor.ToString
+        CompanyLabel.Content = My.Application.Info.Copyright
+        Label1.Content = My.Application.Info.CompanyName
+    End Sub
+
+    Private Sub OKButton_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles OKButton.Click
+        Close()
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles Button1.Click
+        Process.Start("http://semagsoft.com")
+    End Sub
+End Class
